82ed88ab3e536ab24de9d7259c38673655e2d1bb
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2024-06-03 17:02:26 -0300

    2

--- actividad_clase06.py
+++ actividad_clase06.py
@@ -45,3 +45,65 @@
 #      if contador ==10:
 #       print("estamos en el 10") 
 #       continue
+# def saludar(nombre):
+#  print(f"Hola {nombre}")
+# saludar("gustavo")
+
+
+
+# matriz=[
+#  [1,2,3],
+#  [1,1,2],
+#  [1,1,1]
+# ]
+# print(matriz[0][2])
+# matriz.__add__(1)
+
+# lista=[1,2,3,56,85,2,12,56,3,589,156,125]
+# print(max(lista))
+# lista_ordenada=sorted(lista)
+# print(lista_ordenada)
+
+# lista_ordenada=sorted(lista,reverse=True)
+# print(lista_ordenada)
+# print(lista)
+# nueva_lista = lista.sort()
+# print(nueva_lista)
+
+# def viaje():
+#     precio_conbustible = int(input("ingrese precio del conbustible:"))
+#     km_recorridos = int(input("ingrese cantidad de km recorridos: "))
+#     tiempo_del_viaje= float(input("ingrese el tiempo que duro el viaje: "))
+#     velocidad_promedio = km_recorridos / tiempo_del_viaje
+#     consumo_combustible = km_recorridos / 100 * 10
+#     print(f"el consumo de combustible es de en litros es {consumo_combustible}")
+#     print(f"la velocidad promedio es de {velocidad_promedio} km/h")
+#     consumo_100km=precio_conbustible*100
+#     print(f"el consumo por 100 km es de ${consumo_100km}")
+#     por_km= consumo_combustible / 100  
+#     print(f"el consumo por km es de ${por_km}")
+#     por_km_peso =
+
+def busqueda_lineal(lista,odjetivo):
+    return odjetivo in lista
+
+
+# numero=[1,5,3]
+# while True:
+ 
+#  objetivo=int(input("ingrese un numero que desea buscar: "))
+
+#  if busqueda_lineal(numero,objetivo):
+#     print("si esta")
+#  else :
+#     print("no esta")
+#  uso= str(input(" desea usar de nuevo la busqueda s/n?: ")).lower()
+#  if uso == "n":
+#   break
+
+
+def suma(*args):
+    total=sum(args)
+    print(total)
+
+suma(2,3,4,5,6,7,7)
